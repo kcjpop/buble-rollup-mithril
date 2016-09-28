5907dc6be73b06748ce5e876323889fc4134ccb3
An Cao <kcj.pop.com@gmail.com>
2016-09-28 14:50:55 +0300

    Fix bundle path, use buble rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
+import filesize from 'rollup-plugin-filesize'
+import buble from 'rollup-plugin-buble'
 
 export default {
   entry: 'src/index.js',
   format: 'cjs',
   plugins: [
-    nodeResolve({
-      browser: true
-    }),
-    commonjs({
-      include: 'node_modules/**'
-    })
+    buble(),
+    nodeResolve({browser: true}),
+    commonjs({include: 'node_modules/**'})
+  ].concat(
+    process.env.NODE_ENV === 'production'
+    ? [
+      filesize()
+    ] : []
+  ),
+  targets: [
+    {format: 'cjs', dest: 'public/built/index.js'}
   ]
 }
