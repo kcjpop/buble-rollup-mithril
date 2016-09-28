7fd0d0c9b633e22479ecdbcad48cb18c54e1b060
An Cao <kcj.pop.com@gmail.com>
2016-09-28 15:55:34 +0300

    Define a Layout component

--- src/home/index.js
+++ src/home/index.js
@@ -1,9 +1,9 @@
 import m from 'mithril'
 import SearchForm from 'home/el/search-form'
 
-const view = vnode => m('.u-window-box--medium',
+const view = vnode => [
   m('h1.c-header', 'Hello World'),
   m(SearchForm)
-)
+]
 
 export default {view}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import m from 'mithril'
 import Home from 'home/index'
 import Users from 'users/index'
 
+const Layout = {
+  view (vnode) {
+    return m('.u-window-box--medium', vnode.children)
+  }
+}
+
+const layoutize = subComponent => () => m(Layout, m(subComponent))
+
 m.route(document.body, '/', {
-  '/': Home,
-  '/users/:username': Users
+  '/': {render: layoutize(Home)},
+  '/users/:username': {render: layoutize(Users)}
 })
