e4883d54c70aa13ffe36383402ba44aeeba3d694
An Cao <kcj.pop.com@gmail.com>
2016-09-29 14:27:07 +0300

    Init work for postcss plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,17 @@ import commonjs from 'rollup-plugin-commonjs'
 import filesize from 'rollup-plugin-filesize'
 import includePaths from 'rollup-plugin-includepaths'
 import nodeResolve from 'rollup-plugin-node-resolve'
+import postcss from 'rollup-plugin-postcss'
+import postcssModules from 'postcss-modules'
 import uglify from 'rollup-plugin-uglify'
 import { minify } from 'uglify-js'
+
+const cssExportMap = {}
 
 export default {
   entry: 'src/index.js',
   format: 'cjs',
   plugins: [
-    buble(),
     includePaths({
       paths: ['src']
     }),
@@ -19,7 +22,20 @@ export default {
     }),
     commonjs({
       include: 'node_modules/**'
-    })
+    }),
+    postcss({
+      plugins: [
+        postcssModules({
+          getJSON (id, exportTokens) {
+            cssExportMap[id] = exportTokens
+          }
+        })
+      ],
+      getExport (id) {
+        return cssExportMap[id]
+      }
+    }),
+    buble()
   ].concat(
     process.env.NODE_ENV === 'production'
     ? [
--- src/pages/hello.js
+++ src/pages/hello.js
@@ -1,5 +1,6 @@
 import m from 'mithril'
+import style from './style.css'
 
-const view = () => m('h1', 'Hello World')
+const view = () => m(`h1.${style.header}`, 'Hello World')
 
 export default {view}
